984e361a8c11bc9a8f7973222646bc7df0ce9f3b
dobryi <dobryi.roman@mail.ru>
2016-03-03 13:27:38 +0200

    VCS changed

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -3,19 +3,20 @@ import random
 
 def quick_sort(seq):
     n = len(seq)
-    pivot, left, right = seq[int(n // 2)], 0, n - 1
-    while left <= right:
-        print(pivot, left, right)
-        while seq[left] < pivot and left < n:
-            left += 1
-        while seq[right] > pivot and right > -1:
-            right -= 1
-        if left <= right:
-            seq[left], seq[right] = seq[right], seq[left]
+    if n == 1:
+        return seq
+    pivot, pivot_i = seq[0], 0
+    for i in range(1, n):
+        if seq[i] < pivot:
+            seq[pivot_i], seq[i] = seq[i], seq[pivot_i]
+            pivot_i = i
+
 
     return seq
 
-test_seq = [random.randint(-20, 20) for _ in range(20)]
+
+#test_seq = [random.randint(-20, 20) for _ in range(20)]
+test_seq = [9, 6, 3, 4, 10, 8, 2, 7]
 print(test_seq)
 sorted_test_seq = quick_sort(test_seq.copy())
 print(sorted_test_seq)
